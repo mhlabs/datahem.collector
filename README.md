e9a44da7709e56b318ba922ff744a47748d59d1d
Robert Sahlin <robert.sahlin@mathem.se>
2018-06-11 11:53:13 +0200

    endpoints working w get and post

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # datahem.collector
 
-mvn endpoints-framework:openApiDocs -Dendpoints.hostname=$PROJECT_ID.appspot.com
+mvn endpoints-framework:openApiDocs -Dendpoints.project.id=$PROJECT_ID
 gcloud endpoints services deploy target/openapi-docs/openapi.json
 mvn -Dendpoints.project.id=$PROJECT_ID appengine:deploy
--- src/main/java/org/datahem/echo/Echo.java
+++ src/main/java/org/datahem/echo/Echo.java
@@ -54,6 +54,31 @@ import com.google.api.server.spi.config.Named;
 import com.google.api.server.spi.config.Nullable;
 import com.google.api.server.spi.response.UnauthorizedException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
+
+import org.datahem.protobuf.collector.v1.CollectorPayloadEntityProto.*;
+import org.datahem.collector.utils.PubSubHelper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.stream.Stream;
+import java.util.Enumeration;
+import java.util.UUID;
+
+import com.google.apphosting.api.ApiProxy;
+import org.joda.time.Instant;
+
 /**
  * The Echo API which Endpoints will be exposing.
  */
@@ -66,22 +91,13 @@ import com.google.api.server.spi.response.UnauthorizedException;
         ownerDomain = "echo.datahem.org",
         ownerName = "echo.datahem.org",
         packagePath = ""
-    )/*,
-    // [START_EXCLUDE]
-    issuers = {
-        @ApiIssuer(
-            name = "firebase",
-            issuer = "https://securetoken.google.com/YOUR-PROJECT-ID",
-            jwksUri =
-                "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system"
-                    + ".gserviceaccount.com"
-        )
-    }*/
-// [END_EXCLUDE]
+    )
 )
 // [END echo_api_annotation]
 
 public class Echo {
+	private static final Logger LOG = LoggerFactory.getLogger(Echo.class);
+	private static final List<String> HEADERS = Stream.of("X-AppEngine-Country","X-AppEngine-Region","X-AppEngine-City","X-AppEngine-CityLatLong","user-agent").collect(Collectors.toList());
 
   /**
    * Echoes the received message back. If n is a non-negative integer, the message is copied that
@@ -94,58 +110,54 @@ public class Echo {
    * depending on the API method name. In this case, the HTTP method will default to POST.
    */
   // [START echo_method]
-  @ApiMethod(name = "echo")
-  public Message echo(Message message, @Named("n") @Nullable Integer n) {
-    return doEcho(message, n);
-  }
-  // [END echo_method]
+	@ApiMethod(name = "post", path = "collect/{stream}", httpMethod = ApiMethod.HttpMethod.POST)
+	public void collect_post(HttpServletRequest req, Message message, @Named("stream") String stream) throws IOException {
+		String payload = message.getMessage();
+			if (!"".equals(payload)) {
+				buildCollectorPayload(payload, req, stream);
+			}
+	}
+// [END echo_method]
 
-  /**
-   * Echoes the received message back. If n is a non-negative integer, the message is copied that
-   * many times in the returned message.
-   *
-   * <p>Note that name is specified and will override the default name of "{class name}.{method
-   * name}". For example, the default is "echo.echo".
-   *
-   * <p>Note that httpMethod is not specified. This will default to a reasonable HTTP method
-   * depending on the API method name. In this case, the HTTP method will default to POST.
-   */
-  // [START echo_path]
-  @ApiMethod(name = "echo_path_parameter", path = "echo/{n}")
-  public Message echoPathParameter(Message message, @Named("n") int n) {
-    return doEcho(message, n);
-  }
-  // [END echo_path]
+// [START echo_method]
+	@ApiMethod(name = "get", path = "collect/{stream}", httpMethod = ApiMethod.HttpMethod.GET)
+	public void collect_get(HttpServletRequest req, @Named("stream") String stream) throws IOException{
+		String payload = req.getQueryString();        
+		//Check if post body contains payload and create a proto
+		if (!"".equals(payload)) {
+			LOG.info("payload: " + payload);
+			buildCollectorPayload(payload, req, stream);
+		}
+}
+// [END echo_method]
 
-  /**
-   * Echoes the received message back. If n is a non-negative integer, the message is copied that
-   * many times in the returned message.
-   *
-   * <p>Note that name is specified and will override the default name of "{class name}.{method
-   * name}". For example, the default is "echo.echo".
-   *
-   * <p>Note that httpMethod is not specified. This will default to a reasonable HTTP method
-   * depending on the API method name. In this case, the HTTP method will default to POST.
-   */
-  // [START echo_api_key]
-  @ApiMethod(name = "echo_api_key", path = "echo_api_key", apiKeyRequired = AnnotationBoolean.TRUE)
-  public Message echoApiKey(Message message, @Named("n") @Nullable Integer n) {
-    return doEcho(message, n);
-  }
-  // [END echo_api_key]
+private static void buildCollectorPayload(String payload, HttpServletRequest req, String stream) throws IOException{
+		List<CollectorPayloadEntity> collectorPayloadEntities = new ArrayList<>();
+		long timestampMillis = Instant.now().getMillis();
 
-  private Message doEcho(Message message, Integer n) {
-    if (n != null && n >= 0) {
-      StringBuilder sb = new StringBuilder();
-      for (int i = 0; i < n; i++) {
-        if (i > 0) {
-          sb.append(" ");
-        }
-        sb.append(message.getMessage());
-      }
-      message.setMessage(sb.toString());
-    }
-    return message;
-  }
- 
+		//Use application id to get project id (first remove region prefix, i.e. s~ or e~)
+		String pubSubProjectId = ApiProxy.getCurrentEnvironment().getAppId().replaceFirst("^[a-zA-Z]~", "");
+		String uuid = UUID.randomUUID().toString();
+		
+		Enumeration<String> headerNames = req.getHeaderNames();
+
+		Map<String, String> headers = Collections
+			.list(headerNames)
+			.stream()
+			.filter(s -> HEADERS.contains(s)) //Filter out sensitive fields and only keep those specified in HEADERS
+			.map(s -> new String[]{s, req.getHeader(s)})
+			.collect(Collectors.toMap(s -> s[0], s -> s[1]));
+
+			LOG.info(Arrays.toString(headers.entrySet().toArray()));
+
+		CollectorPayloadEntity collectorPayloadEntity = CollectorPayloadEntity.newBuilder()
+			.setPayload(payload)
+			.putAllHeaders(headers)
+			.setEpochMillis(Long.toString(timestampMillis))
+			.setUuid(uuid)
+			.build();
+
+		collectorPayloadEntities.add(collectorPayloadEntity);
+		PubSubHelper.publishMessages(collectorPayloadEntities, pubSubProjectId, stream);
+	}
 }
